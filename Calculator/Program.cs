e55326b7242e429b8414c950ec0852335085a2b9
Hussain Ghazaly <42726137+HussainGhazaly@users.noreply.github.com>
2023-09-19 12:44:00 +0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,89 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+/*
+ * 
+ * 1) Learn Code Refactoring
+ * 2) Apply all the past Lectures (IF - FUNC - INTPARS )  
+ * 3) how to transform a string to uppercase version
+ * 4) Implement calculator APP
+ * 
+ */
+
+
+Console.WriteLine("Hello");
+
+Console.WriteLine("Input the first number: ");
+var firstAsText = Console.ReadLine();
+var number1 = int.Parse(firstAsText);
+
+
+Console.WriteLine("Input the second number: ");
+var secondAsText = Console.ReadLine();
+var number2 = int.Parse(secondAsText);
+
+Console.WriteLine("what " +
+    "do you want to do? ");
+Console.WriteLine("[A]dd numbers ");
+Console.WriteLine("[S]ubtract numbers ");
+Console.WriteLine("[M]ultiply numbers ");
+
+var choice = Console.ReadLine();
+
+if (EqualsCaseInsensitive(choice , "A") )
+{
+
+    var sum = number1 + number2 ;
+    //Console.WriteLine(number1 + " + " + number2 + " = " + sum);
+
+    PrintFinalEquation(number1 , number2 , sum, "+");
+}
+else if (EqualsCaseInsensitive(choice, "S"))
+{
+
+        var Difference = number1 - number2;
+    //Console.WriteLine(number1 + " - " + number2 + " = " + Difference);
+
+    PrintFinalEquation(number1, number2, Difference, "-");
+    }
+else if (EqualsCaseInsensitive(choice, "M"))
+{
+
+    var multiplied = number1 * number2;
+    //Console.WriteLine(number1 + " * " + number2 + " = " + multiplied);
+
+    PrintFinalEquation(number1, number2, multiplied, "*");
+}
+else
+{
+    Console.WriteLine("Invalid choice! ");
+}
+
+
+/// Note: TO use key C# words as Variables ==
+/// "Add @ before the keyword"
+/// 
+
+// EX : "3 * 4 = 12"
+void PrintFinalEquation (int number1 ,int number2 , int result , string @operator) {
+
+    Console.WriteLine(number1 + " " + @operator + " " + number2 + " = " + result);
+    
+}
+
+bool EqualsCaseInsensitive (string left , string right) { 
+
+    // calling a built in method directly on a string (similar to "left.Length" )
+    // Note ToUpper , will Transform both parameter to uppercase
+    // & see if they are the same ("A" == "A")
+    // to upper is an object of type string ( we will elarn this in CLASS OBJECT ORINTED LEC)
+
+    return left.ToUpper() == right.ToUpper();
+
+}
+
+
+
+Console.WriteLine("press any key to close ");
+Console.ReadKey();
---
+++ TodoList/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Threading.Channels;
+
+/*
+ * 
+ * 
+ * 
+ * // Lec 56 - 60: TODO List Project 
+ * 
+ * 
+ */
+
+
+
+// we will use this (contains  & ADD methods & count ) from list methods 
+var todos = new List<string>();
+
+Console.WriteLine("Hello!");
+
+bool shallExit = false;
+while (!shallExit)
+{
+    Console.WriteLine();
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+
+    var userChoice = Console.ReadLine();
+
+    switch (userChoice)
+    {
+
+        case "E":
+        case "e":
+            shallExit = true;
+            break;
+        case "S":
+        case "s":
+            SeeAllTodos();
+            break;
+        case "A":
+        case "a":
+            AddTodo();
+            break;
+        case "R":
+        case "r":
+            RemoveTodo();
+            break;
+        default:
+            Console.WriteLine("Invalid Choice");
+            break;
+
+    }
+}
+Console.ReadKey();
+
+
+
+
+
+// 1) method to Display and see all todo
+void SeeAllTodos()
+{
+    if (todos.Count == 0)
+    {
+        ShowNoTodosMessage();
+        return;
+    }
+
+    for (int i = 0; i < todos.Count; i++)
+    {
+        // use string interpolation, to print index followed by a dot & then by todo description
+        Console.WriteLine($"{i + 1}. {todos[i]}"); // print all todo list
+    }
+
+
+
+
+}
+
+// 2) Method to Add a todo list 
+void AddTodo()
+{
+    string description;
+    do
+    {
+        Console.WriteLine("Enter the Todo desciption: ");
+        description = Console.ReadLine();
+    }
+    while (!IsDescriptionValid(description));
+
+    todos.Add(description);
+}
+
+
+// 3) Method to Remove a todo list ,
+// note: list index start from 0 and count-1 , but her we are starting from 1
+
+void RemoveTodo()
+{
+
+    if (todos.Count == 0)
+    {
+        ShowNoTodosMessage();
+        return;
+    }
+
+    int index; // loop until in dex unsuccessful 
+    do
+    {
+        Console.WriteLine("Select the index of the TODO you want to remove");
+        SeeAllTodos();
+    } while (!TryReadIndex(out index));
+
+    RemoveTodoAtIndex(index - 1);
+}
+
+
+
+
+
+
+// refactor to check if Description is Valid (  check if empty + check if the user enter same message)
+bool IsDescriptionValid(string description)
+{
+
+    // check if the discription valid
+    if (description == "") // check if empty " " 
+    {
+        Console.WriteLine("The description cannot be empty");
+        return false;
+    }
+    if (todos.Contains(description)) // to put condition , check if the user enter same message
+    {
+        Console.WriteLine("The discrption must be uniqe. ");
+        return false;
+    }
+    return true;
+}
+
+// refactor for reading the index that we want to remove it
+bool TryReadIndex(out int index) // method to read the index
+{
+    var userInput = Console.ReadLine();
+    if (userInput == "") // check if its empty
+    {
+        index = 0;
+        Console.WriteLine("Selected index cannot be empty!");
+        return false;
+    }
+    if (int.TryParse(userInput, out index) && index >= 1 && index <= todos.Count) //select valid index 
+    {
+
+        return true;
+    }
+    Console.WriteLine("the given index is not valid. ");
+    return false;
+}
+
+// refactor for remove todo by index number
+void RemoveTodoAtIndex(int index)
+{
+    var todoToBeRemoved = todos[index];
+    todos.RemoveAt(index);
+    Console.WriteLine("TODO removed: " + todoToBeRemoved);
+}
+
+
+
+// refactor for replacing "No To do Message"
+static void ShowNoTodosMessage()
+{
+    Console.WriteLine("No todos have been added yet.");
+}
