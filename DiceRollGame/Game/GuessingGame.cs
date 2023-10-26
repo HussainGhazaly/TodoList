5bc42e276a57caf633211d9a8ad93d4de5ce356f
Hussain Ghazaly <42726137+HussainGhazaly@users.noreply.github.com>
2023-10-26 13:27:03 +0300

    New Project Added (Guess The Rolling Dice)

---
+++ DiceRollGame/Game/GuessingGame.cs
@@ -0,0 +1,48 @@
+﻿
+
+using DiceRollGame.UserCommunication;
+
+namespace DiceRollGame.Game
+{
+    public class GuessingGame
+    {
+        private readonly Dice _dice;
+        private const int InitialTries = 3;
+
+        public GuessingGame(Dice dice)
+        {
+            _dice = dice;
+        }
+
+        public GameResult Play()
+        {
+            var diceRollResult = _dice.Roll(); //start rolling :))
+            Console.WriteLine($"Dice rolled. Guess what number it shows in {InitialTries} tries. ");
+
+            var triesLeft = InitialTries;
+            while (triesLeft > 0)
+            {
+                var guess = ConsoleReader.ReadInteger("Enter a number: ");
+                // Now we want to check if the number entered by the user == to the rolled dice :) 
+
+                if (guess == diceRollResult)
+                {
+                    return GameResult.Victory;
+                }
+                Console.WriteLine("Wrong number. ");
+                --triesLeft;
+            }
+            return GameResult.Loss;
+
+        }
+
+        public static void PrintResult(GameResult gameResult)
+        {
+            // Ternary condtion 3 oprands
+            string message = gameResult == GameResult.Victory
+                ? "You win!"
+                : "You Lose :(";
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ DiceRollGame/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+
+using DiceRollGame.Game;
+
+var random = new Random();
+var dice = new Dice(random);
+var guessingGame = new GuessingGame(dice);
+
+GameResult gameResult = guessingGame.Play();
+GuessingGame.PrintResult(gameResult);
+
+
+Console.ReadKey();
+
+
+
+
+
+
+/*
+ * 
+ * 
+ *   // Enum type is a type that defines a set if names constants ,
+ *   (we will use it to create a better name for bool)
+ * 
+ * */
+
+
+//Season firstSeason = Season.Spring;
+//Season lastSeason = Season.Winter;
+//int firstSeasonAsNumber = (int)firstSeason;
+//int LastSeasonAsNumber = (int)lastSeason;
+
+//Console.WriteLine(firstSeason);
+//Console.WriteLine("As a number: " + firstSeasonAsNumber);
+
+//Console.WriteLine(lastSeason);
+//Console.WriteLine("As a number: " + LastSeasonAsNumber);
+
+//Console.ReadKey();
+
+//public enum Season
+//{
+//    Spring = 1, //0
+//    Summer, //1
+//    Autumn, //2
+//    Winter //3
+//}
+
+//public enum HttpCode
+//{
+//    ok =200,
+//    NotFound = 404,
+//    InternalServerError = 500
+//}
+
+
+
