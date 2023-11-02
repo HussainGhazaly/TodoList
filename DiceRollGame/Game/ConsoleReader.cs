189dbd5e1401a3d4290a635abb458d0192f10f93
Hussain Ghazaly <42726137+HussainGhazaly@users.noreply.github.com>
2023-11-02 14:43:28 +0300

    Overriding members from the base class. Virtual methods and properties

---
+++ DiceRollGame/Game/ConsoleReader.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace DiceRollGame.Game
+{
+    // (static = use itselfe fixed ,  cannot create instance)
+    // static class Only contain Static methods
+
+    public static class ConsoleReader
+    {
+        public static int ReadInteger(string meassage)
+        {
+            int result;
+            do
+            {
+                Console.WriteLine(meassage);
+            }
+            while (!int.TryParse(Console.ReadLine(), out result));
+            return result;
+        }
+    }
+
+
+}
--- Poly-Inherit-Interfaces/Program.cs
+++ Poly-Inherit-Interfaces/Program.cs
@@ -1,4 +1,25 @@
-﻿Console.ReadKey();
+﻿
+//var pizza = new Pizza();
+//pizza.AddIngredient(new Cheddar());
+//pizza.AddIngredient(new Mozarella());
+//pizza.AddIngredient(new Cheddar());
+
+//Console.WriteLine(pizza.Describe());
+
+var ingredient = new Ingredient();
+ingredient.PublicField = 10;
+
+var Cheddar = new Cheddar();
+Cheddar.PublicField = 20;
+
+Console.WriteLine("Value in Cheddar: " + Cheddar.PublicField);
+Console.WriteLine("Value in Ingredient: " + ingredient.PublicField);
+
+//Console.WriteLine(Cheddar.PublicMethod());
+//Console.WriteLine(Cheddar.PrivateMethod());
+//Console.WriteLine(Cheddar.ProtectedMethod());
+
+Console.ReadKey();
 
 public class Pizza
 {
@@ -13,27 +34,40 @@ public class Pizza
 
 public class Ingredient
 {
-    public string Name { get; }
-    public int AgedForMonths { get; }
-    public int TomatoIn100Grams { get; }
-    public int IsLight  { get; }
+    public int PublicField;
+
+    public string PublicMethod() =>
+         "This method is Public in the Ingredient class.";
+    protected string ProtectedMethod() => // can be used in the drived classes + Base class , but not outside  
+      "This method is Protected in the Ingredient class.";
+
+    private string PrivateMethod() => // for Private : we can only use it inside the base class (No Drive ,No Outside)
+        "This method is Private in the Ingredient class.";
 }
 
-public class Cheddar
+public class Cheddar : Ingredient
 {
     public string Name => "Cheddar cheese";
     public int AgedForMonths { get; }
+
+    public void UseMethodsFromBaseClass()
+    {
+        Console.WriteLine(PublicMethod());
+        Console.WriteLine(ProtectedMethod());
+       // Console.WriteLine(PrivateMethod());
+    }
+
 }
 
 
-public class TomatoSauce
+public class TomatoSauce : Ingredient
 {
     public string Name => "Tomato Sauce";
     public int TomatoIn100Grams { get; }
 }
 
 
-public class Mozarella
+public class Mozarella : Ingredient
 {
     public string Name => "Mozarella";
     public int IsLight { get; }
