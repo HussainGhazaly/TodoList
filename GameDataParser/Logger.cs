cc9418c1474d5d3bc0cdc2fc4c5e864f11f0dfbe
Hussain Ghazaly <42726137+HussainGhazaly@users.noreply.github.com>
2023-12-06 12:30:27 +0300

    162. Assignment - Game Data Parser - Custom logger

---
+++ GameDataParser/Logger.cs
@@ -0,0 +1,21 @@
+﻿public class Logger
+{
+    private readonly string _logFileName;
+
+    public Logger(string logFileName)
+    {
+        _logFileName = logFileName;
+    }
+
+    public void log(Exception ex) 
+    {
+        var entry =
+        $@"[{DateTime.Now}]
+        Execption message: {ex.Message}
+        Stack trace: {ex.StackTrace}
+
+
+        ";
+        File.AppendAllText(_logFileName,entry);
+    }
+}
--- GameDataParser/Program.cs
+++ GameDataParser/Program.cs
@@ -1,50 +1,86 @@
 ﻿
 using System.Text.Json;
 
-bool isFileRead = false;
-var fileContents= default(string);
-do
+var app = new GameDataParserApp();
+var logger = new Logger("log.txt");
+
+try 
 {
-    try
-    {
-        Console.WriteLine("Enter the name of the file you want to read:");
-        var fileName = Console.ReadLine();
-
-        fileContents = File.ReadAllText(fileName);
-        isFileRead = true;
-    }
-    catch (ArgumentNullException ex)
-    {
-        Console.WriteLine("the file name cannot be null.");
-    }
-    catch (ArgumentException ex)
-    {
-        Console.WriteLine("the file name cannot be empty.");
-    }
-    catch (FileNotFoundException ex)
-    {
-        Console.WriteLine("the file does not exist.");
-    }
-} while (!isFileRead);
-
-var videoGames = JsonSerializer.Deserialize<List<VideoGame>>(fileContents); // deserializing (converting JSON to objects).
-
-if (videoGames.Count > 0) 
-{
-    Console.WriteLine();
-    Console.WriteLine("Loaded games are: ");
-    foreach (var videoGame in videoGames) 
-    {
-        Console.WriteLine(videoGame);
-    }
+    app.Run();
 }
-else 
+catch(Exception ex)
 {
-    Console.WriteLine("No games are present in the input file. ");
+    Console.WriteLine("Sorry! the application has experienced an unexpected error " + "and will have to be closed.");
+    logger.log(ex);
 }
 
+Console.WriteLine("Press any key to close.");
 Console.ReadKey();
 
+public class GameDataParserApp
+{ 
+    public void Run() 
+    {
+
+        bool isFileRead = false;
+        var fileContents = default(string);
+        var fileName = default(string);
+        do
+        {
+            try
+            {
+                Console.WriteLine("Enter the name of the file you want to read:");
+                fileName = Console.ReadLine();
+
+                fileContents = File.ReadAllText(fileName);
+                isFileRead = true;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("the file name cannot be null.");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("the file name cannot be empty.");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("the file does not exist.");
+            }
+        } while (!isFileRead);
+
+        List<VideoGame> videoGames = default;
+        try
+        {
+            videoGames = JsonSerializer.Deserialize<List<VideoGame>>(fileContents); // deserializing (converting JSON to objects).
+        }
+        catch (JsonException ex)
+        {
+            var originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"JSON in {fileName} file was not " + $"in a valid format. JSON body:");
+            Console.Write(fileContents);
+            Console.ForegroundColor = originalColor;
+
+            throw new JsonException($"{ex.Message} The file is: {fileName}", ex);
+        }
+
+        if (videoGames.Count > 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Loaded games are: ");
+            foreach (var videoGame in videoGames)
+            {
+                Console.WriteLine(videoGame);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No games are present in the input file. ");
+        }
+
+    }
+}
 public class VideoGame
 {
     public string Title { get; init; }
